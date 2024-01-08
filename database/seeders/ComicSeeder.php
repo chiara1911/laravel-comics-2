f35972318cb53468d13dd05a17d41e69b51987a8
chiara1911 <chiaracesari89@gmail.com>
2024-01-08 22:42:45 +0100

    stamp all element in db in phpmyadmin

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -12,8 +13,23 @@ class ComicSeeder extends Seeder
      * @return void
      */
 
+
+     //vado a prendere l'array comincs nel db.php
      public function run()
      {
         //
+        $data = config("db.comics");
+        // dd($data);
+        foreach($data as $value){
+            $newComic = new Comic();
+            $newComic ->title = $value["title"];
+            $newComic ->description = $value["description"];
+            $newComic ->thumb = $value["thumb"];
+            $newComic-> price = $value["price"];
+            $newComic->sale_date = $value["sale_date"];
+            $newComic->series = $value["series"];
+            $newComic->type = $value["type"];
+            $newComic->save();
+        }
      }
 }
