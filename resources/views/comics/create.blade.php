f9e326b8d9b9275ee20d2c85be833efe869d0697
chiara1911 <chiaracesari89@gmail.com>
2024-01-09 00:13:06 +0100

    add input and text area for create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -8,22 +8,34 @@
         $items = config('items.items');
     @endphp
     <main>
-{{-- creo un form che mi salva i dati di questa pagina, nella route per salvare che è comics.store mettendo il metodo post--}}
+        {{-- creo un form che mi salva i dati di questa pagina, nella route per salvare che è comics.store mettendo il metodo post --}}
+
+        <section class="mx-5 pt-4">
 
 
-<form action="{{ route('comics.store') }}" method="POST">
+            <form action="{{ route('comics.store') }}" method="POST">
 
-            @csrf  {{-- <-- token , va messo sempre ogni volta che abbiamo un form per ragioni di sicurezza (cioè vengono da una pagina del nostro sito)--}}
-            <input type="text" id="title" name="title" placeholder="inserisci un titolo" class="form-control">
-            <input type="text" id="description" name="description" placeholder="inserisci una descrizione"
-                class="form-control">
-            <input type="text" id="price" name="price" placeholder="inserisci un prezzo" class="form-control">
-            <input type="text" id="type" name="type" placeholder="inserisci un tipo" class="form-control">
-            <button type="submit">Invia
+                @csrf {{-- <-- token , va messo sempre ogni volta che abbiamo un form per ragioni di sicurezza (cioè vengono da una pagina del nostro sito) --}}
+                <input type="text" id="title" name="title" placeholder="inserisci un titolo" class="form-control mt-2">
+                <textarea class="form-control mt-2" type="text" id="description" name="description"
+                    placeholder="inserisci una descrizione" rows="4"></textarea>
 
-            </button>
-        </form>
+                <input type="text" id="thumb" name="thumb" placeholder="inserisci un'immagine"
+                    class="form-control mt-2 w-50">
+                    <input type="text" id="price" name="price" placeholder="inserisci il prezzo"
+                    class="form-control mt-2 w-25">
+                <input type="text" id="sale_date" name="sale_date" placeholder="inserisci la data di uscita"
+                    class="form-control mt-2 w-25">
+                <input type="text" id="series" name="series" placeholder="inserisci una serie"
+                    class="form-control mt-2 w-25">
+                    <input type="text" id="type" name="type" placeholder="inserisci un tipo"
+                    class="form-control mt-2 w-25">
+                <button type="submit" class="mt-4 btn btn-success">Invia
 
+                </button>
+            </form>
+
+        </section>
 
     </main>
 @endsection
