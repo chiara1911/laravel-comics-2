27d2ac0be68df6afb460cffa1417e3477cc2b309
chiara1911 <chiaracesari89@gmail.com>
2024-01-08 22:32:23 +0100

    add migrate in phpmyadmin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,21 +19,24 @@ Route::get('/', function () {
     return view('home' , compact('comics'));
 });
 
-//altre pagine
-Route::get('/comics' , function(){
-    $comics = config('db.comics');
-    return view('comics.index', compact('comics'));
-}) -> name('comics.index');
 
-Route::get('/comics/{comic}', function ($id) {
-    $comics = config('db.comics');
-    if ($id >= 0 && $id < count($comics)) {
-        $comic = $comics[$id];
-        return view('comics.show', compact('comic'));
-    } else {
-        abort(404);
-    }
-})->name('comics.show');
+//altre pagine
+// Route::get('/comics' , function(){
+//     $comics = config('db.comics');
+//     return view('comics.index', compact('comics'));
+// }) -> name('comics.index');
+
+// Route::get('/comics/{comic}', function ($id) {
+//     $comics = config('db.comics');
+//     if ($id >= 0 && $id < count($comics)) {
+//         $comic = $comics[$id];
+//         return view('comics.show', compact('comic'));
+//     } else {
+//         abort(404);
+//     }
+// })->name('comics.show');
+
+Route::resource('comics', ComicController::class);
 
 Route::get('/movies' , function(){
     $movies = config('moviedb.movies');
